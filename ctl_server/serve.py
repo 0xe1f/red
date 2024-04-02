2a244da969602f159381b45f6540c2618e97a984
Akop Karapetyan <2538706+0xe1f@users.noreply.github.com>
2024-04-01 23:02:43 -0700

    server

--- ctl_server/serve.py
+++ ctl_server/serve.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from jinja2 import Environment, FileSystemLoader
+import glob
+import http.server
+import http
+import jinja2
 import json
 import logging
 import mimetypes
-import re
+import os
+import urllib.parse
+import yaml
 
-class S(BaseHTTPRequestHandler):
+template_path = 'templates'
+templates = {}
+games = []
+
+class Handler(http.server.BaseHTTPRequestHandler):
 
     def write_response_headers(self, code = 200, type = 'text/html'):
         self.send_response(code)
@@ -16,26 +24,32 @@ class S(BaseHTTPRequestHandler):
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
 
-        m = simple_path_capture.match(self.path)
-        if m and m.group(1) == 'static':
-            self.serve_static_file("{}/{}".format(m.group(1), m.group(2)))
-            return
+        path = os.path.normpath(self.path).lstrip('/')
+        if path.startswith('static/'):
+            self.serve_static_file(path)
+        else:
+            template = templates.get(path if path else 'index')
+            if not template:
+                self.send_error(http.HTTPStatus.NOT_FOUND, "Not found")
+                return
 
-        self.write_response_headers()
-        content = template.render(
-            games = games,
-        )
-        self.wfile.write(content.encode('utf-8'))
+            content = template.render(
+                games = games
+            )
+            self.write_response_headers()
+            self.wfile.write(content.encode('utf-8'))
 
     def do_POST(self):
-        m = simple_path_capture.match(self.path)
-        if m and m.group(1) == 'action':
-            self.handle_action(m.group(2))
+        path = urllib.parse.urlparse(self.path).path.lstrip('/')
+        if path.startswith('action'):
+            self.handle_action(path.rsplit('/', 1)[-1])
+        else:
+            self.send_error(http.HTTPStatus.NOT_FOUND, "Not found")
+            return
 
         self.write_response_headers()
 
     def serve_static_file(self, path):
-        # TODO: Cache type
         logging.info("serve_static_file: %s", path)
         mt = mimetypes.guess_type(path)
         self.write_response_headers(type = mt[0])
@@ -55,9 +69,8 @@ class S(BaseHTTPRequestHandler):
     def launch(self, id):
         logging.info('launch: %s', id)
 
-
-def run(server_class=HTTPServer, handler_class=S, port=8080):
-    logging.basicConfig(level=logging.INFO)
+def run(server_class = http.server.HTTPServer, handler_class = Handler, port = 8080):
+    logging.basicConfig(level = logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     logging.info('Starting httpd...\n')
@@ -68,24 +81,31 @@ def run(server_class=HTTPServer, handler_class=S, port=8080):
     httpd.server_close()
     logging.info('Stopping httpd...\n')
 
-environment = Environment(loader=FileSystemLoader("templates/"))
-template = environment.get_template("main.jinja")
-simple_path_capture = re.compile('/([a-zA-Z0-9_.]{1,64})/([a-zA-Z0-9_.]{1,64})')
-
 class Game:
-    def __init__(self, id, title):
-        self.id = id
-        self.title = title
+    def __init__(self, **item):
+        self.__dict__.update(item)
 
-games = [
-    Game('pacman', 'Pacman'),
-    Game('digdug', 'Dig Dug'),
-]
+def init_templates():
+    global templates
+    environment = jinja2.Environment(loader = jinja2.FileSystemLoader(template_path))
+    for file in glob.glob("{}/*.jinja".format(template_path)):
+        base = os.path.basename(file)
+        name = os.path.splitext(base)[0]
+        templates[name] = environment.get_template(base)
+
+def init_games():
+    global games
+    with open("games.yaml") as fd:
+        for item in yaml.safe_load(fd):
+            games.append(Game(**item))
+
+init_templates()
+init_games()
 
 if __name__ == '__main__':
     from sys import argv
 
     if len(argv) == 2:
-        run(port=int(argv[1]))
+        run(port = int(argv[1]))
     else:
         run()
