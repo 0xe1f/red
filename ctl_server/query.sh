ed7db6da7e1afa92d1dc7a162b4d448083e366ac
Akop Karapetyan <2538706+0xe1f@users.noreply.github.com>
2024-04-06 13:32:04 -0700

    Scrim

--- ctl_server/query.sh
+++ ctl_server/query.sh
@@ -4,10 +4,11 @@
 ## 2024, Akop Karapetyan
 
 HOST=$1
+EMULATOR_EXE="fbneo"
 
 if [ -z "${HOST}" ]; then
     echo "Missing game host" >&2
     exit
 fi
 
-ssh "${HOST}" "pgrep -a fbneo" | awk '{print $3}'
+ssh "${HOST}" "pgrep -a ${EMULATOR_EXE}" | awk '{print $3}'
--- ctl_server/static/main.js
+++ ctl_server/static/main.js
@@ -10,10 +10,12 @@ $(function() {
     $(".game").on("click", function() {
         const item = $(this);
         const payload = { 'id': item.data('id') };
+        $('#scrim').show();
         $.post(
             "launch",
             JSON.stringify(payload),
             function() {
+                $('#scrim').hide();
                 $('.game.selected').removeClass('selected');
                 item.addClass('selected');
             },
