ad22e5527f87b70567bdbf8e58ca33097dcba9f6
Akop Karapetyan <2538706+0xe1f@users.noreply.github.com>
2024-04-06 18:42:09 -0700

    Submodules

--- deploy.py
+++ deploy.py
@@ -11,6 +11,11 @@ import yaml
 CTL_SERVER_EXE='serve.py'
 CTL_SERVER_PATH='ctl_server'
 
+class GameServer:
+
+    def __init__(self, **item):
+        self.__dict__.update(item)
+
 class GameClient:
 
     def __init__(self, **item):
@@ -19,20 +24,18 @@ class GameClient:
 parser = argparse.ArgumentParser()
 parser.add_argument('--ctl', action='store_true', help='Deploy to control server')
 parser.add_argument('--cli', action='store_true', help='Deploy to clients')
+parser.add_argument('--svr', action='store_true', help='Deploy to game server')
 parser.add_argument('--all', action='store_true', help='Deploy everything')
 args = parser.parse_args()
 
-if not (args.ctl or args.cli or args.all):
+if not (args.ctl or args.svr or args.cli or args.all):
     sys.exit('Specify at least one option or "--all" to deploy everything')
 
 with open('deploy.yaml') as fd:
     yaml = yaml.safe_load(fd)
-    # game_server = yaml['game_server']
+    game_server = GameServer(**yaml['game_server'])
     game_clients = [GameClient(**x) for x in yaml['game_clients']]
     ctl_server_host = yaml['ctl_server']
-
-# if not game_server:
-#     sys.exit('Missing game server configuration')
 
 if args.ctl or args.all:
     if not ctl_server_host:
@@ -54,6 +57,31 @@ if args.ctl or args.all:
         f'{ctl_server_host}',
         f"kill `ps -ef | awk '{{ print $2\"\t\"$8\"\t\"$9 }}' | grep python | grep {CTL_SERVER_EXE} | cut -f 1` 2> /dev/null; " \
         f'cd {CTL_SERVER_PATH}; nohup ./{CTL_SERVER_EXE} >log.txt 2>&1 &',
+    ])
+
+if args.svr or args.all:
+    if not game_server:
+        sys.exit('Missing game server configuration')
+
+    print("Deploying to server...")
+    subprocess.call([
+        'rsync',
+        '-vrt',
+        '--exclude', '*.exclude',
+        '--exclude-from', 'game_servers/common.exclude',
+        '--exclude-from', 'game_servers/fbneo.exclude',
+        f'game_servers/',
+        f'{game_server.host}:{game_server.path}',
+    ])
+
+    print("Building server(s)...")
+    subprocess.call([
+        'ssh',
+        '-o',
+        'StrictHostKeyChecking no',
+        f'{game_server.host}',
+        '-t',
+        f'cd {game_server.path} && ./build.sh',
     ])
 
 if args.cli or args.all:
---
+++ game_servers/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Building fbneo" >&2
+
+cd fbneo && make ss
