8025deabe3fee6818368c13e4f303e1e02748923
Akop Karapetyan <2538706+0xe1f@users.noreply.github.com>
2024-04-14 20:47:34 -0700

    Running sticky

--- ctl_server/static/main.js
+++ ctl_server/static/main.js
@@ -1,23 +1,37 @@
 $(function() {
+    const updateSelection = function(id) {
+        $('.game.selected').removeClass('selected');
+        if (!id) {
+            return;
+        }
+        const $item = $(`.game[data-id="${id}"]`);
+        if ($item.length) {
+            $item.addClass('selected');
+            $('#running .title').text(
+                $item.children('.title').text()
+            );
+            $('#running').show();
+        }
+    };
     $.get(
         "query",
         function(resp) {
             if (resp.title) {
-                $('.game[data-id="' + resp.title + '"]').addClass('selected');
+                updateSelection(resp.title);
             }
         },
     );
     $(".game").on("click", function() {
         const item = $(this);
-        const payload = { 'id': item.data('id') };
+        const itemId = item.data('id');
+        const payload = { 'id': itemId };
         $('#scrim').show();
         $.post(
             "launch",
             JSON.stringify(payload),
             function() {
                 $('#scrim').hide();
-                $('.game.selected').removeClass('selected');
-                item.addClass('selected');
+                updateSelection(itemId);
             },
             "json",
         );
